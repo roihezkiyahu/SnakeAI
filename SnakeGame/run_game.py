1c7dbdff647d8402ab778aa42472f9208b9bc6ee
roihezkiyahu <rhezki11@gmail.com>
2024-03-26 19:03:49 +0200

    reorder code after commit init

--- SnakeGame/run_game.py
+++ SnakeGame/run_game.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import time
 import keyboard
 from snake_game import SnakeGame  # Make sure to save the SnakeGame class code in a file named snake_game.py
@@ -39,45 +38,3 @@ def print_game_state(game):
 
 if __name__ == "__main__":
     play_game()
-=======
-import time
-import keyboard
-from snake_game import SnakeGame  # Make sure to save the SnakeGame class code in a file named snake_game.py
-
-def play_game():
-    game = SnakeGame()
-
-    # Set up key press events to change direction
-    keyboard.on_press_key("up", lambda _: game.change_direction('UP'))
-    keyboard.on_press_key("down", lambda _: game.change_direction('DOWN'))
-    keyboard.on_press_key("left", lambda _: game.change_direction('LEFT'))
-    keyboard.on_press_key("right", lambda _: game.change_direction('RIGHT'))
-
-    while not game.game_over:
-        # Move the snake
-        score, game_over = game.move()
-
-        # Clear the screen and print the game state
-        print("\033c", end="")  # This escape sequence clears the screen in most terminals
-        print(f"Score: {score}")
-        print_game_state(game)
-
-        # Adjust the sleep time to control the speed of the game
-        time.sleep(0.2)
-
-
-def print_game_state(game):
-    for y in range(game.height):
-        for x in range(game.width):
-            if (x, y) == game.food:
-                print("F", end="")
-            elif (x, y) in game.snake:
-                print("S", end="")
-            else:
-                print(".", end="")
-        print()
-
-
-if __name__ == "__main__":
-    play_game()
->>>>>>> f191d8d8c3fdd9808cca7d2035b93a2a10cac58b
--- modeling/memory.py
+++ modeling/memory.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 from collections import namedtuple, deque
 
@@ -18,25 +17,4 @@ class ReplayMemory(object):
         return random.sample(self.memory, batch_size)
 
     def __len__(self):
-=======
-import random
-from collections import namedtuple, deque
-
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
->>>>>>> f191d8d8c3fdd9808cca7d2035b93a2a10cac58b
         return len(self.memory)
